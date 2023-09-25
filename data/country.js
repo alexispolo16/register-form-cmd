3b7846a5beb68be4700de10845563b0b3a87ce5f
Eficads <support@eficads.com>
2023-09-25 07:14:30 -0500

    first commit

--- data/country.js
+++ data/country.js
@@ -1,10 +1,33 @@
 const axios = require('axios');
 
-// Función que obtiene la lista de países
+// Función que obtiene la lista de países con Ecuador, Colombia y Perú al principio
 async function getCountries() {
   try {
     const response = await axios.get('https://restcountries.com/v3.1/all');
     const countries = response.data.map((country) => country.name.common);
+
+    // Ordena los países alfabéticamente
+    countries.sort((a, b) => a.localeCompare(b));
+
+    // Mueve Ecuador, Colombia y Perú al principio
+    const colombiaIndex = countries.indexOf('Colombia');
+    if (colombiaIndex !== -1) {
+      countries.splice(colombiaIndex, 1);
+      countries.unshift('Colombia');
+    }
+
+    const peruIndex = countries.indexOf('Peru');
+    if (peruIndex !== -1) {
+      countries.splice(peruIndex, 1);
+      countries.unshift('Peru');
+    }
+
+    const ecuadorIndex = countries.indexOf('Ecuador');
+    if (ecuadorIndex !== -1) {
+      countries.splice(ecuadorIndex, 1);
+      countries.unshift('Ecuador');
+    }
+
     return countries;
   } catch (error) {
     console.error('Error al obtener la lista de países:', error);
