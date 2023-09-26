dbc49a03ed51f494e5dc70b80c36786992fe9d91
Eficads <support@eficads.com>
2023-09-25 19:24:35 -0500

    no mas cambios

--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,7 @@ export default function Home() {
           pais: formData.country,
           ciudad: formData.city,
           profesion: formData.userType,
-          terms: formData.terms,
+          terms: "True",
         },
       };
 
@@ -128,7 +128,41 @@ export default function Home() {
       <Head>
         <title>AWS Community Day Ecuador</title>
         <meta name="description" content="Evento Organizado por la comunidad de AWS UG Ecuador" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KZHT51K5PP"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KZHT51K5PP');
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N339JR9V');
+            `,
+          }}
+        />
+      
       </Head>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-N339JR9V"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
       <img src="https://assets.awsugecuador.com/assets/banner1.png" />
       <br />
       <div className="max-w-screen-md max-w-md p-4 shadow-xl">
@@ -143,7 +177,7 @@ export default function Home() {
           <div className="flex flex-wrap -mx-2">
             <div className="w-full md:w-1/2 px-2 mb-4">
               <label htmlFor="firstName" className="block text-gray-700">
-                Nombre:
+                Nombres:
               </label>
               <input
                 type="text"
@@ -157,7 +191,7 @@ export default function Home() {
             </div>
             <div className="w-full md:w-1/2 px-2 mb-4">
               <label htmlFor="lastName" className="block text-gray-700">
-                Apellido:
+                Apellidos:
               </label>
               <input
                 type="text"
